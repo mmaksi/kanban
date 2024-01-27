caa0d14a39f925e3ce9df5410c5f5143eeaa17b7
mmaksi <mmaksi.dev@gmail.com>
2024-01-27 17:10:50 +0100

    fixed minor bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import * as actions from "@/actions/actions";
 import { Navbar } from "@/components/Navbar.component";
 import { Sidebar } from "@/components/Sidebar.component";
 import { Board } from "@/components/Board.component";
+import { BoardData } from "@/types/schemas";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const boards = await actions.getAllBoards();
+  const boards = (await actions.getAllBoards()) as unknown as BoardData[];
 
   return (
     <div>
