a730be1d7d62a3e142dbe5532949eeef04ed9e00
mmaksi <mmaksi.dev@gmail.com>
2024-01-30 18:57:24 +0100

    adding pwa

--- next.config.js
+++ next.config.js
@@ -7,13 +7,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   reloadOnOnline: true,
   swcMinify: true,
   dest: "public",
-  fallbacks: {
-    //image: "/static/images/fallback.png",
-    document: "/offline", // if you want to fallback to a custom page rather than /_offline
-    // font: '/static/font/fallback.woff2',
-    // audio: ...,
-    // video: ...,
-  },
   workboxOptions: {
     disableDevLogs: true,
   },
