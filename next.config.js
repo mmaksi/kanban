94579d93722d9fee0a1370d77cfe5fa590218f9e
mmaksi <mmaksi.dev@gmail.com>
2024-01-30 18:47:44 +0100

    adding pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,30 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
-module.exports = {
+const withPWA = require("@ducanh2912/next-pwa").default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    document: "/offline", // if you want to fallback to a custom page rather than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
 };
+
+module.exports = withPWA(nextConfig);
